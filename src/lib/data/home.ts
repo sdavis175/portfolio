b2bb85cea48167601785785bd6064fed2a2b11f0
sdavis175 <ruunanaa@gmail.com>
2024-09-08 15:34:59 +0900

    Fixing gh-pages

--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -8,7 +8,7 @@ export const name = 'Shane';
 export const lastName = 'Davis';
 
 export const description =
-	'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, rerum. Debitis accusantium deleniti enim iste dignissimos? Similique, exercitationem! Odit vero, numquam quae ratione maxime sunt reiciendis laudantium quaerat iure ipsum!';
+	'description';
 
 export const links: Array<{ platform: Platform; link: string }> = [
 	{
