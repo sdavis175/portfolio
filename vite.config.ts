080ea5bc697fabb460d550a6827d0f839899989b
sdavis175 <ruunanaa@gmail.com>
2024-09-11 00:05:58 +0900

    Testing HTTPS deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import UnoCSS from 'unocss/vite';
 import mkcert from 'vite-plugin-mkcert';
 
 const config: UserConfig = {
-	plugins: [UnoCSS(), sveltekit(), mkcert()]
+	plugins: [UnoCSS(), sveltekit(), mkcert()],
+	base: "https://www.shanedavisinc.com/portfolio/"
 };
 
 export default config;
