8fd60a996994fbd467c9757c9d7df096366583a0
sdavis175 <ruunanaa@gmail.com>
2024-09-11 00:11:04 +0900

    Removing HTTPS deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import UnoCSS from 'unocss/vite';
 import mkcert from 'vite-plugin-mkcert';
 
 const config: UserConfig = {
-	plugins: [UnoCSS(), sveltekit(), mkcert()],
-	base: "https://www.shanedavisinc.com/"
+	plugins: [UnoCSS(), sveltekit()]
 };
 
 export default config;
