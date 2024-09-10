04f215699e49c9e4c7b1139e705066a557f2eaf8
sdavis175 <ruunanaa@gmail.com>
2024-09-11 00:03:24 +0900

    Testing HTTPS deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 import UnoCSS from 'unocss/vite';
+import mkcert from 'vite-plugin-mkcert';
 
 const config: UserConfig = {
-	plugins: [UnoCSS(), sveltekit()]
+	plugins: [UnoCSS(), sveltekit(), mkcert()]
 };
 
 export default config;
