4faf543c455a2d7fb0f7ede5e6344e27c2aaf179
sdavis175 <ruunanaa@gmail.com>
2024-09-16 22:04:02 +0900

    Fixed variable names.

--- src/lib/data/navbar.ts
+++ src/lib/data/navbar.ts
@@ -2,7 +2,7 @@ import { selectedLanguage } from '@stores/language';
 import { writable } from 'svelte/store';
 import type { NavBar } from '$lib/types';
 
-const items_en: Array<NavBar> = [
+const enItems: Array<NavBar> = [
 	{ title: 'Skills', to: '/skills', icon: 'i-carbon-software-resource-cluster' },
 	{ title: 'Projects', to: '/projects', icon: 'i-carbon-cube' },
 	{ title: 'Experience', to: '/experience', icon: 'i-carbon-development' },
@@ -10,7 +10,7 @@ const items_en: Array<NavBar> = [
 	{ title: 'Resumé', to: '/resume', icon: 'i-carbon-result' }
 ];
 
-const items_ja: Array<NavBar> = [
+const jaItems: Array<NavBar> = [
 	{ title: 'スキール', to: '/skills', icon: 'i-carbon-software-resource-cluster' },
 	{ title: 'プロジェクト', to: '/projects', icon: 'i-carbon-cube' },
 	{ title: '経験', to: '/experience', icon: 'i-carbon-development' },
@@ -28,13 +28,13 @@ selectedLanguage.subscribe((language) => {
 	switch (language) {
 		case 'en':
 			navbarData.set({
-				items: items_en,
+				items: enItems,
 			});
 			break;
 
 		case 'ja':
 			navbarData.set({
-				items: items_ja,
+				items: jaItems,
 			});
 			break;
 
