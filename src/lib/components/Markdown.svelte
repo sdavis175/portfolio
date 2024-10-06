76ad4b793c3c4ffd5edf54a9fdb9ac1b49775b1d
sdavis175 <ruunanaa@gmail.com>
2024-10-06 10:32:19 +0900

    Fixed SSR issue preventing build.

--- src/lib/components/Markdown.svelte
+++ src/lib/components/Markdown.svelte
@@ -6,24 +6,29 @@
 	import { marked } from 'marked';
 	import 'prismjs/components/prism-typescript';
 	import 'prismjs/themes/prism-tomorrow.css';
+	import { onMount } from 'svelte';
+	import DOMPurify from 'dompurify';
 
 	let container: HTMLDivElement;
-
 	export let content: string;
 
-	const sanitizer = createSanitizer(window);
+	// Declare the sanitizer variable with the correct type
+	let sanitizer: DOMPurify.DOMPurifyI | null = null;
 
-	// Reactive statement that runs whenever `content` changes
-	$: if (container && content) {
-		marked.use(gfmHeadingId());
-		marked.use(mangle());
+	// Initialize and configure marked and DOMPurify once
+	onMount(() => {
+		if (typeof window !== 'undefined') {
+			marked.use(gfmHeadingId());
+			marked.use(mangle());
+			sanitizer = createSanitizer(window);
+		}
+	});
 
+	// Reactive block: This updates the content whenever `content` changes
+	$: if (sanitizer && content) {
 		const parsed = marked.parse(content);
-
 		container.innerHTML = sanitizer.sanitize(parsed);
-
-		// Highlight the syntax
-		Prism.highlightAllUnder(container);
+		Prism.highlightAllUnder(container); // Re-apply syntax highlighting
 	}
 </script>
 
